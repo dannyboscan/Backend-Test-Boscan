83c6ce712d7dde5a17d8bcda310228949fde3ba9
Danny Boscan <dannyboscan@gmail.com>
2020-10-13 20:15:45 -0300

    API tests

--- app/tests/mealorders/test_api.py
+++ app/tests/mealorders/test_api.py
@@ -1,7 +1,11 @@
+import uuid
+import json
+import pendulum
+import pytest
 from django.urls import reverse
-
+from django.core import serializers
 from mealorders.models import (
-    SlackSetting
+    SlackSetting, Dish, Menu, EmployeeOrder
 )
 
 
@@ -57,6 +61,60 @@ def test_get_single_salcksetting(admin_client):
     assert res.data["token"] == token
 
 
+def test_get_slack_setting_unauthorized(client, django_user_model):
+    username = "username"
+    password = "password"
+    django_user_model.objects.create_user(username=username, password=password)
+    client.login(username=username, password=password)
+    res = client.get(
+        reverse("mealorders:api_slack_setting-list")
+    )
+
+    assert res.status_code == 403
+
+
+def test_get_channels(admin_client):
+    res = admin_client.post(
+        reverse("mealorders:api_slack_setting-channels"),
+        {"token": "xoxb-1420668090691-1405724482855-RWdZwDjjmD22pFIfSgr4a3ma"}
+    )
+
+    assert res.status_code == 200
+    assert res.data["ok"]
+
+
+def test_get_channels_no_token(admin_client):
+    res = admin_client.post(
+        reverse("mealorders:api_slack_setting-channels")
+    )
+
+    assert res.status_code == 400
+
+
+def test_get_channels_unauthorized(client, django_user_model):
+    username = "username"
+    password = "password"
+    django_user_model.objects.create_user(username=username, password=password)
+    client.login(username=username, password=password)
+    res = client.post(
+        reverse("mealorders:api_slack_setting-channels")
+    )
+
+    assert res.status_code == 403
+
+
+def test_get_channels_invalid_token(admin_client):
+    res = admin_client.post(
+        reverse("mealorders:api_slack_setting-channels"),
+        {"token": str(uuid.uuid4())}
+    )
+
+    print(res.data)
+    assert res.status_code == 200
+    assert not res.data["ok"]
+    assert res.data["error"] == "invalid_auth"
+
+
 def test_get_single_slacksetting_incorrect_id(admin_client):
     res = admin_client.get(
         reverse("mealorders:api_slack_setting-detail", kwargs={'pk': 0})
@@ -71,11 +129,26 @@ def test_get_all_slacksetting(admin_client):
     assert res.status_code == 200
 
 
-def test_get_all_dish(admin_client):
+def test_get_all_dishes(admin_client):
     res = admin_client.get(
         reverse("mealorders:api_dishes-list",)
     )
     assert res.status_code == 200
+
+
+def test_get_filtered_dishes(admin_client):
+    Dish.objects.bulk_create([
+        Dish(name="Pastel de choclo, Ensalada y Postre"),
+        Dish(name="Arroz con nugget de pollo, Ensalada y Postre"),
+        Dish(name="Arroz con hamburguesa, Ensalada y Postre"),
+        Dish(name="Ensalada premium de pollo y Postre"),
+    ])
+
+    res = admin_client.get(
+        f"{reverse('mealorders:api_dishes-list',)}?q=arroz con"
+    )
+    assert res.status_code == 200
+    assert res.data['count'] == 2
 
 
 def test_get_all_menu(admin_client):
@@ -85,8 +158,96 @@ def test_get_all_menu(admin_client):
     assert res.status_code == 200
 
 
+def test_create_menu(admin_client):
+    dishes = Dish.objects.bulk_create([
+        Dish(name="Pastel de choclo, Ensalada y Postre"),
+        Dish(name="Arroz con nugget de pollo, Ensalada y Postre"),
+        Dish(name="Arroz con hamburguesa, Ensalada y Postre"),
+        Dish(name="Ensalada premium de pollo y Postre"),
+    ])
+
+    res = admin_client.post(
+        reverse("mealorders:api_menus-list"),
+        {
+            "dishes": [d.id for d in dishes],
+            "date": pendulum.now().subtract(days=1).format("YYYY-MM-DD"),
+            "reminder_sent": True
+        }
+    )
+
+    assert res.status_code == 201
+    assert len(res.data['dishes']) == len(dishes)
+    assert not res.data['reminder_sent']
+
+
+def test_menu_send_reminder_unauthorized(client, django_user_model):
+    username = "username"
+    password = "password"
+    django_user_model.objects.create_user(username=username, password=password)
+    client.login(username=username, password=password)
+    res = client.post(
+        reverse("mealorders:api_menus-send-reminder", kwargs={"pk": str(uuid.uuid4)})
+    )
+
+    assert res.status_code == 403
+
+
+def test_menu_send_reminder(admin_client):
+    menu = Menu.objects.create(date=pendulum.now().date())
+
+    res = admin_client.post(
+        reverse("mealorders:api_menus-send-reminder", kwargs={"pk": menu.id})
+    )
+
+    assert res.status_code == 200
+    assert res.data["ok"]
+
+
+def test_menu_send_reminder_invalid(admin_client):
+    menu = Menu.objects.create(date=pendulum.now().add(days=1).date())
+
+    res = admin_client.post(
+        reverse("mealorders:api_menus-send-reminder", kwargs={"pk": menu.id})
+    )
+
+    assert res.status_code == 200
+    assert not res.data["ok"]
+
+
 def test_get_all_menuorder(admin_client):
     res = admin_client.get(
         reverse("mealorders:api_orders-list",)
     )
     assert res.status_code == 200
+
+
+@pytest.mark.django_db
+def test_place_order(client):
+    assert not EmployeeOrder.objects.exists()
+
+    dishes = Dish.objects.bulk_create([
+        Dish(name="Arroz con nugget de pollo, Ensalada y Postre"),
+        Dish(name="Arroz con hamburguesa, Ensalada y Postre"),
+    ])
+
+    menu = Menu.objects.create(date=pendulum.now().date())
+    menu.dishes.add(*dishes)
+
+    print (dishes[0].id)
+    res = client.post(
+        reverse("mealorders:api_employee_order-list"),
+        json.dumps({
+            "menu": menu.id,
+            "full_name": "nombre",
+            "email": "correo@example.com",
+            "dishes": [{
+                "dish": dishes[0].id,
+                "quantity": 1
+            }]
+        }, cls=serializers.json.DjangoJSONEncoder),
+        content_type='application/json'
+    )
+
+    print (res.data)
+    assert res.status_code == 201
+    assert EmployeeOrder.objects.exists()
